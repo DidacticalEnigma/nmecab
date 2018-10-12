83a101c0d9defe35da6f7489caf8b2dc309bee63
milleniumbug <milleniumbug@protonmail.com>
2018-10-12 21:32:49 +0200

    Add information for NuGet packaging purposes

--- src/LibNMeCabNetStandard/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("LibNMeCab")]
-[assembly: AssemblyDescription("")]
-[assembly: ComVisible(false)]
-[assembly: Guid("C9521EC7-758E-4293-B543-F017A1C9E5A9")]
